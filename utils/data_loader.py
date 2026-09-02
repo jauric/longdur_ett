db6e325b44cc510239d081c6f5fbfcdb87e1a107
Johannes Aurich <aurich-engineering@protonmail.com>
2026-09-02 02:58:44 +0200

    Restructuring repo

---
+++ utils/data_loader.py
@@ -0,0 +1,115 @@
+import pandas as pd
+import numpy as np
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+
+
+class Sample(Dataset):
+
+    '''
+    Sampling for multivariate-to-univariate(OT) one-shot forecasting.
+    '''
+
+    def __init__(self, data: pd.DataFrame, context_length: int, forecast_length: int):
+
+        # Converting DataFrame to 32-bit float torch.tensor for better processing, going through a np.array for compatibility reasons
+        self.feature_data    = torch.from_numpy(data.to_numpy(dtype=np.float32))          # input x -- either the whole training, validation or testing split
+        self.target_data     = torch.from_numpy(data["OT"].to_numpy(dtype=np.float32))    # prediction target y = whole OT time series
+
+        self.context_length  = context_length       # L 
+        self.forecast_length = forecast_length      # H 
+
+
+    def __len__(self):
+
+        # Total number of samples in the dataset
+        sample_qty = (self.feature_data.shape[0] - self.context_length - self.forecast_length) + 1      # N = ((T-L-H)/S)+1     | Stride S = 1 timestep
+
+        return sample_qty
+
+
+    def __getitem__(self, sample_index):
+        # Construct the sample from the input data
+
+        # x = sample input data = the full multivariate time series but restricted to the context length = only ranging from index to (index + context_length) (index = 0,1...)
+        x_inputs_array = self.feature_data[sample_index : (sample_index + self.context_length)]           # last value of that range is EXCLUDED, the first is INCLUDED
+
+        # y = data of the correlated target output -- here: univariate output of OT, but positioned behind the the training window and being as wide as the forecast window
+        y_targets_array = self.target_data[(sample_index + self.context_length) : (sample_index + self.context_length + self.forecast_length)]     # MIND! last value of that range is EXCLUDED, the first is INCLUDED
+
+        return x_inputs_array, y_targets_array
+
+
+
+
+def initialize_dataloaders(
+        prepared_data: tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, dict], 
+        context_length: int, 
+        forecast_length: int,
+        batch_size: int, 
+        training_shuffle: bool
+        ) -> tuple[DataLoader, DataLoader, DataLoader]:
+
+    '''
+    Convert split and scaled data into PyTorch DataLoaders.
+
+    Args:
+        prepared_data: the output of function in data_utils.py: data_prep() -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, dict]
+
+    Returns:
+        out: tuple of the batched samples for training, validation, and test; each with x_inputs and y_targets
+    '''
+    
+
+    # Initialize data sampling
+    training_dataset = Sample(
+        data            = prepared_data[0],
+        context_length  = context_length,
+        forecast_length = forecast_length
+    )
+
+    validation_dataset = Sample(
+        data            = prepared_data[1],
+        context_length  = context_length,
+        forecast_length = forecast_length
+    )
+
+    test_dataset = Sample(
+        data            = prepared_data[2],
+        context_length  = context_length,
+        forecast_length = forecast_length
+    )
+
+    # Load samples and initialize batches
+    training_batches = DataLoader(
+        dataset    = training_dataset,
+        batch_size = batch_size,
+        shuffle    = training_shuffle
+    )
+
+    validation_batches = DataLoader(
+        dataset    = validation_dataset,
+        batch_size = batch_size,
+        shuffle    = False
+    )
+
+    test_batches = DataLoader(
+        dataset    = test_dataset,
+        batch_size = batch_size,
+        shuffle    = False
+    )
+
+    # Shape check: extract batches for x_inputs and y_targets data
+    x_inputs_batch, y_targets_batch = next(iter(training_batches))
+
+    print("Input shape:", x_inputs_batch.shape)
+    print("Target shape:", y_targets_batch.shape)
+    print("Number of batches:", len(training_batches))
+
+
+    return training_batches, validation_batches, test_batches
+    # each batch = x_inputs, y_targets, with 
+    # x_inputs.shape  = [batch_size, len = context_length, channels = 7], 
+    # y_targets.shape = [batch_size, len = forecast_length] -> univariate, no channel dimension
---
+++ utils/data_utils.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import numpy as np
+
+
+
+def data_import(url: str) -> pd.DataFrame:
+
+    data = pd.read_csv(url, sep = ",")
+    data.set_index('date', inplace=True)
+
+    return data
+
+
+
+def data_prep(
+        source_data: pd.DataFrame, 
+        train_length_months: int, 
+        vali_length_months: int, 
+        test_length_months: int, 
+        context_length: int,
+        scaler: object
+        
+        ) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, dict]:
+
+    '''
+    Converting the source DataFrame into split and scaled datasets, while returning the scalers.
+    Currently only for ETTh data with 1h intervals, can be extended to cover ETTm1 with 15min steps.
+    
+    Args:
+        scaler: enter a scaler object from e.g. sklearn.preprocessing, incl. freature_ranges if required.
+    '''
+
+    ############### Creating raw data splits
+
+    # Converting months into hours to match the source data
+    hours_per_month   = 30 * 24     # hours
+
+    training_length   = train_length_months * hours_per_month         # hours
+    validation_length = vali_length_months * hours_per_month          # hours
+    test_length       = test_length_months * hours_per_month          # hours
+
+    # Create training, validation, and test splits
+    training_split    = source_data.iloc[:training_length]
+    validation_split  = source_data.iloc[training_length:(training_length + validation_length)]
+    test_split        = source_data.iloc[(training_length + validation_length):(training_length + validation_length + test_length)]
+    discarded_split   = source_data.iloc[(training_length + validation_length + test_length):]
+
+    timesteps = training_split.shape[0] + validation_split.shape[0] + test_split.shape[0]
+    timesteps_source = source_data.shape[0]         # Number of rows in the source data set
+
+    print("Timesteps in the source dataset: ", timesteps_source, "hours")
+    print("Timesteps in the truncated split: ", timesteps, "hours")
+    print("Timesteps in the training split: ", training_split.shape[0], "hours")
+    print("Timesteps in the discarded split: ", discarded_split.shape[0], "hours")
+    print("The training/validation/test split relative to the truncated set [%]: ", 
+          training_split.shape[0]/timesteps*100, "/", validation_split.shape[0]/timesteps*100, "/", test_split.shape[0]/timesteps*100)
+
+
+    ############### Assembling the actual data sets from the splits, considering moving window sampling
+
+    # No changes needed
+    training_set = training_split
+
+    # Extending the validation split, x_val should start at -context in the training set, so first val_prediction is the first unseen entry in the validation split (moving window, see Dataset __getitem__!)
+    validation_set = pd.concat([ training_split.iloc[ -context_length : ], validation_split])
+
+    # Extending the testing split, respectively
+    test_set = pd.concat([ validation_split.iloc[ -context_length : ], test_split])
+
+
+    ############### Scaling the data
+
+    # Set up empty DataFrames to hold the scaled data with the right index and columns; 0 = training_set, 1 = validation_set, 2 = test_set
+    training_set_scaled   = pd.DataFrame(index = training_set.index, columns = training_set.columns)
+    validation_set_scaled = pd.DataFrame(index = validation_set.index, columns = validation_set.columns)
+    test_set_scaled       = pd.DataFrame(index = test_set.index, columns = test_set.columns)
+
+    # Initialize a dictionary to store the specific scaler for each channel
+    scalers_dict = {}
+
+    for column in training_set.columns:
+
+        # Define and fit the scaler on the training set of each signal only
+        scaler.fit(training_set[[column]])
+
+        # Save this column's scaler for later inverse transformation of predictions
+        scalers_dict[column] = scaler
+
+        # Transform the training, validation, and test sets of each signal using the fitted scaler. All set have the same index and columns as the original data frames.
+        training_set_scaled[column] = scaler.transform(training_set[[column]])
+        validation_set_scaled[column] = scaler.transform(validation_set[[column]])
+        test_set_scaled[column] = scaler.transform(test_set[[column]])
+
+
+    return training_set_scaled, validation_set_scaled, test_set_scaled, scalers_dict
+    # each scaled set has shape [len, 7]
+
+
+
+def data_rescaling_h():
+    return
+
